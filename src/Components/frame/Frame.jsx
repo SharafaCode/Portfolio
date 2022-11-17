f41eef43bf012de5799967a8dbee440f291f6153
sharafa bashiru <sharafaoluwatobi@gmail.com>
2022-11-17 10:59:37 +0100

    redesign; making frame as component to be use multiple times.

---
+++ src/Components/frame/Frame.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './frame.css'
+
+const Frame = () => {
+  return (
+    <section className='frame-section'>
+        <article className='frame-article'>
+
+            
+
+        </article>
+    </section>
+  )
+}
+
+export default Frame
--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Frame from '../frame/Frame';
 import { BsArrowDown } from "react-icons/bs";
 import './hero.css';
 import Header from '../header/Header';
@@ -8,95 +9,9 @@ import A from '../../assets/20221103_082341.jpg'
 const Hero = () => {
   return (
 
-    <section className='hero-container'>
+   <Frame>
 
-        <article className='hero-wrapper'>
-
-            <div className="hero-content">
-
-              <Header/>
-              <div className='hero-content-wrapper'>
-
-              <div className='name'>
-
-                <div className='subname'>
-
-                  <div>
-                    <p>
-
-                        Hello, I'm
-
-                    </p>
-
-                  </div>
-                  <div>
-
-                    <h3>
-                      SHARAFA
-                    </h3>
-
-                  </div>
-              
-                  </div>
-
-                    <h1>
-                      OLUWATOBI <br />
-                      BASHIR'U
-                    </h1>
-                    <h4>
-                      FrontEnd Web Developer
-                    </h4>
-
-                  </div>
-
-
-                  <div className='btn-wrapper'>
-
-                    <div>
-
-                      <button className='btn'>
-                        About Me
-                      </button>
-
-                    </div>
-
-                    <div>
-                      <button className='btn'>
-                        Profolio
-                      </button>
-                    </div>
-
-
-                  </div>
-
-
-
-                  <div className="image">
-                    <img src={img} alt=""  className='img'/>
-                  </div>
-
-                </div>
-
-              <div className=' cta-btn'>
-                <div className='cta-btn-icon'>
-
-                  < BsArrowDown />
-
-                </div>
-                <span className='cta-btn-text'>
-                  <p>
-                    Keep <br /> Scrolling
-                  </p>
-
-                </span>
-              </div>
-
-            </div>
-
-
-
-        </article>
-    </section>
+   </Frame>
   )
 }
 
