5a6137efda8263e6dfc750cdd4ccf795cdd05c5e
sharafa bashiru <sharafaoluwatobi@gmail.com>
2022-11-21 10:01:46 +0100

    hero content added to hero section

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { BsArrowDown } from "react-icons/bs";
 import './hero.css';
 import Header from '../header/Header';
-import img from '../../assets/IMG_1689.jpg';
-import A from '../../assets/20221103_082341.jpg'
+import BashImage from '../../assets/IMG_1689.jpg';
+
 
 const Hero = () => {
   return (
@@ -11,7 +11,76 @@ const Hero = () => {
     <article className='frame-article'>
     <Header/>
       <div className="hero-content">
-        
+
+        <div className="name-wrapper">
+
+        <div className="subname">
+          
+          <div>
+            <p>
+              Hello, I'm
+            </p>
+          </div>
+          <div>
+            <h3>
+              SHARAFA
+            </h3>
+          </div>
+
+        </div>
+
+        <div className="name">
+          <div>
+
+          <h1>
+            OLUWATOBI
+          </h1>
+
+          </div>
+
+          <div>
+            <h1>
+              BASHIRU
+            </h1>
+          </div>
+        </div>
+        </div>
+
+        <div className="btn">
+          <div>
+            <button type='button' className='btns'>
+              About Me
+            </button>
+
+          </div>
+          <div>
+            <button type='button' className='btns'>
+              Profolio
+            </button>
+          </div>
+        </div>
+
+        <div className="image">
+
+          <img src={BashImage} className='img' alt="bash" />
+
+        </div>
+        <div className="scrollbtn">
+
+          <div className='scrollIcon'>
+
+          <BsArrowDown/>
+          </div>
+          <div>
+            <p>
+              Keep <br />
+              Scrolling
+            </p>
+          </div>
+
+
+        </div>
+      
        
       </div>
     </article>
